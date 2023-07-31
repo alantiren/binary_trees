00c4eee61ef706bae285c56a0bc12270e8ceda02
alantiren <tirenalan0@gmail.com>
2023-07-31 22:26:50 +0300

    header

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,10 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,19 +15,33 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
-
-/* Function to create a binary tree node */
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+/*  linked list for advanced 101 the levelorder */
+/**
+ * struct link_s - structure for advanced tasks
+ *
+ * @n: depth of node specified
+ * @node: node of tree to store
+ * @next: next node of the linked list
+ */
+typedef struct link_s
+{
+	size_t n;
+	struct binary_tree_s const *node;
+	struct link_s *next;
+} link_t;
+/* Function binary tree node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
-
-/* Function to print a binary tree */
 void binary_tree_print(const binary_tree_t *);
 
 

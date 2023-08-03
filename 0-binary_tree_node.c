f6ceaeebaf9351d2f130d52b0fbb965bfb4b115f
alantiren <tirenalan0@gmail.com>
2023-08-03 13:10:19 +0300

    updates

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,16 +11,15 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *newNode;
-
-newNode = malloc(sizeof(binary_tree_t));
-if (newNode == NULL)
+binary_tree_t *new_node;
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
 {
 return (NULL);
 }
-newNode->parent = parent;
-newNode->n = value;
-newNode->right = newNode->left = NULL;
-
-return (newNode);
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+return (new_node);
 }

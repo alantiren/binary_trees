1bb0e41ebe329548d51f11eb05e6bc3c93846ff3
alantiren <tirenalan0@gmail.com>
2023-08-03 16:05:33 +0300

    updates

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,22 +8,21 @@
  */
 size_t binary_tree_height_b(const binary_tree_t *tree)
 {
-	size_t a = 0;
-	size_t b = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		if (tree)
-		{
-			a = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
-			b = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
-		}
-		return ((a > b) ? a : b);
-	}
+size_t a = 0;
+size_t b = 0;
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+if (tree)
+{
+a = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+b = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+}
+return ((a > b) ? a : b);
+}
 }
 
 /**
@@ -34,13 +33,12 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int right = 0, left = 0, total = 0;
-
-	if (tree)
-	{
-		left = ((int)binary_tree_height_b(tree->left));
-		right = ((int)binary_tree_height_b(tree->right));
-		total = left - right;
-	}
-	return (total);
+int right = 0, left = 0, total = 0;
+if (tree)
+{
+left = ((int)binary_tree_height_b(tree->left));
+right = ((int)binary_tree_height_b(tree->right));
+total = left - right;
+}
+return (total);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,12 +9,48 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int left_height, right_height;
+int result = 0;
 if (tree == NULL)
 {
 return (0);
 }
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-return (binary_tree_balance(tree) == 0 && left_height == right_height);
+else
+{
+result = tree_is_perfect(tree);
+if (result != 0)
+{
+return (1);
+}
+return (0);
+}
+}
+
+#include "binary_trees.h"
+/**
+ * tree_is_perfect - function that says if a tree is perfect or not
+ * it has to be the same quantity of levels in left as right, and also
+ * each node has to have 2 nodes or none
+ * @tree: tree to check
+ * Return: 0 if is not a perfect or other number that is the level of height
+ */
+int tree_is_perfect(const binary_tree_t *tree)
+{
+int a = 0, b = 0;
+
+if (tree->left && tree->right)
+{
+a = 1 + tree_is_perfect(tree->left);
+b = 1 + tree_is_perfect(tree->right);
+if (b == a && b != 0 && a != 0)
+return (r);
+return (0);
+}
+else if (!tree->left && !tree->right)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
